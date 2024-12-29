cbbf1cc23021e6ee9f9f5ec7459d3826c7512c79
Kenneth Miura <kennethmiura@Kenneths-MacBook-Pro.local>
2024-12-29 14:31:40 -0500

    contact section

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,15 @@
 import type { Preview } from "@storybook/react";
+import '../app/globals.scss';
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      values: [{
+        // Keep in sync with background color in AllVariables.global.scss
+        name: 'Dark', value: '#0a192f'
+      }],
+      default: 'Dark'
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
--- app/ui/contact/contact-section.tsx
+++ app/ui/contact/contact-section.tsx
@@ -1,17 +1,19 @@
+import LinkButton from "@/app/ui/link-button";
+import {clsx} from "clsx";
+import {highlightFont} from "@/app/ui/fonts";
+import styles from './contact-section.module.scss';
+
 export default function ContactSection(){
     // TODO: define shared colors for the buttons, etc in bootstrap
     return (
-        <div>
-        <h3> 04. What's Next?</h3>
-        <h1>Get In Touch</h1>
-        <span>
+        <div className="flex-col flex max-w-2xl text-center items-center">
+        <h3 className={clsx([styles.headerText, "mb-5", highlightFont.className, ])}> 04. What's Next?</h3>
+        <h1 className={clsx([styles.ctaText, "text-5xl mb-5"])}>Get In Touch</h1>
+        <span className="mb-5">
         Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question, or just want to say hi, I'll try my best to get back to you!
 
         </span>
-
-        <button>
-            Say Hello
-        </button>
+        <LinkButton targetUrl="mailto:kenneth.t.miura@gmail.com" text="Say Hello"/>
 
     </div>)
 }
